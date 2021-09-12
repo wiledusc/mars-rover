beba0a2e425a23bd1eccf5f172f1068cd4eece57
William Schoeffel <william_rn@hotmail.com>
2021-09-12 00:37:08 -0300

    Updates

---
+++ precache-manifest.63ac80bb050868793fd16ddc9511c9a5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae349d3578f5d0b1eacbb73d9c42fe4f",
+    "url": "/mars-rover/index.html"
+  },
+  {
+    "revision": "73f7338b8c5bd41ea0d4",
+    "url": "/mars-rover/static/css/2.8caad30d.chunk.css"
+  },
+  {
+    "revision": "ce0561d63cf37fc508f3",
+    "url": "/mars-rover/static/css/main.66fb8c70.chunk.css"
+  },
+  {
+    "revision": "73f7338b8c5bd41ea0d4",
+    "url": "/mars-rover/static/js/2.86a85228.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mars-rover/static/js/2.86a85228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0561d63cf37fc508f3",
+    "url": "/mars-rover/static/js/main.1e63d412.chunk.js"
+  },
+  {
+    "revision": "40469d1440009870f2fd",
+    "url": "/mars-rover/static/js/runtime-main.750a829e.js"
+  },
+  {
+    "revision": "1303b5c4310bcc8453ea9671fb9665e6",
+    "url": "/mars-rover/static/media/check.1303b5c4.png"
+  },
+  {
+    "revision": "d0a0e54dd6c06ae9af07f842677bb847",
+    "url": "/mars-rover/static/media/mars-rover-3.d0a0e54d.jpg"
+  }
+]);
